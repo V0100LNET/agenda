4e3d5ad13786c1f7888ef38af3706301407fa93a
Oswaldo Hernandez <ohernandezv1601@alumno.ipn.mx>
2021-06-20 00:50:01 -0500

    config deployment to Netlify

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://127.0.0.1:5500/api/agenda/'
+    baseURL: 'https://agenda-interview.herokuapp.com/api/agenda/' //SERVER
+    //baseURL: 'http://127.0.0.1:5500/api/agenda/' //LOCAL 
 })
 
 
